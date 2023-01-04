970b0e297e58664e13575b66d9f0f218d9da31b9
elvis huanca <JHuanca_21@outlook.com>
2023-01-04 15:54:52 -0500

    login con auth0 hecho

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
 import Head from 'next/head'
+// import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Home() {
-  
+  // const { user, error, isLoading } = useUser();
+
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>{error.message}</div>;
+  // console.log(user)
+  // if (user) return (
+    // user && (
+    //   <div>
+    //     <img src={user.picture} alt={user.name} />
+    //     <h2>{user.name}</h2>
+    //     <p>{user.email}</p>
+    //     <a href="/api/auth/logout">Logout</a>
+    //   </div>
+    // )
+  //   <>
+  //     {user[`/roles`].includes('administrador') ? (
+  //       <>
+  //         <h2>binevenido admin</h2>
+  //         <a href="dashboard">dashboard</a><br />
+  //         <a href="api/auth/logout">logout</a>
+  //       </>
+  //     ) : (
+  //       <>
+  //         <h1>Bienvenido user</h1>
+  //         <a href="api/auth/logout">logout</a>
+  //       </>
+  //     )}
+  //   </>
+
+  // );
+
   return (
     <div>
       <Head>
@@ -13,10 +44,11 @@ export default function Home() {
         <h1 className='font-bold text-4xl'>
           Hola a todos, esto es NextJS + TypeScript + TailwindCSS
         </h1>
+        <a href="/api/auth/login">Login</a>
       </main>
 
       <footer>
-       
+
       </footer>
     </div>
   )
