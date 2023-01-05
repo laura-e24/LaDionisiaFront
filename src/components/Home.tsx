a8192e653f52c2ece2ec0bad85d280ee11643bde
genesisagostina <111713328+genesisagostina@users.noreply.github.com>
2023-01-04 23:38:39 -0300

    cambio minimo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,6 @@
 const Home = () => {
     return (
-        <div class="container">
-        <div class="cookie-bar__inner">
-          <div class="cookie-bar__text rte"><p>Debes tener la edad legal permitida del consumo de bebidas alcoholicas.</p></div><button type="button" class="cookie-bar__button button button--secondary" data-action="accept-terms">To accept</button></div>
+        <div>
         </div>
     )
 
