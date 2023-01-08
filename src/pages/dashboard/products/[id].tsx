1b8a4af8fb0bb1b403e87e68a7b285f4854776ac
Florencia <florponge@gmail.com>
2023-01-08 13:35:50 -0300

    description almost finished

--- src/pages/dashboard/products/[id].tsx
+++ src/pages/dashboard/products/[id].tsx
@@ -5,61 +5,65 @@ export default function Product({ wine }) {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="w-full h-full flex flex-wrap self-center justify-center divide-x-2 gap-y-8">
-      <img src ={wine.image} alt="" />
-      <div className="flex flex-col items-center text-center font-bold mt-10 mb-5">
-      <h1>{wine.wine}</h1>
+      <div className="w-full h-full flex flex-wrap self-center justify-center">
+        <div className="w-1/5 h-1/5 flex flex-wrap self-center justify-left">
+          <img src={wine.image} alt="" />
+        </div>
+        <div className="w-1/2 h-1/2 flex flex-col items-left text-left font-bold mt-10 mb-5">
+          <h1>{wine.wine}</h1>
+          <p>$100</p>
+          <h5>(5 stars) 10 reviews</h5>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+          <div>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+          </div>
+          <h5>{wine.description}</h5>
+          <p>cantidad</p>
+          <div className="mb-3 pt-0">
+            <input type="text" placeholder="1" className=" text-center shrink w-10 h-14px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
+
+            <button className="w-80 flex flex-col items-center text-center8 bg-btn-color text-white py-4 px-8 hover:bg-red-600" type="button"
+            >
+              Añadir al carrito
+            </button>
+            <br />
+            <button className="w-80 flex flex-col items-center text-center bg-btn-color text-white py-4 px-8 hover:bg-red-600" type="button"
+            >
+              Añadir a lista de deseos
+            </button>
+
+          </div>
+        </div>
       </div>
+
+      <div className="p-5">
+        <h1 className="font-bold mt-10 mb-5r" >RESEÑAS Y PUNTUACIONES</h1>
+        <h2 className="font-bold mt-10 mb-5">Escribi una reseña</h2>
+        <div className="mb-3 pt-0">
+          <input type="text" placeholder="Escribir..." className="shrink w-66 px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
+        </div>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        <h5 className="font-bold mt-10 mb-5" >Puntuar</h5>
+        <span className="heading"></span>
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+        <br />
       </div>
-      <p>$100</p>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-      <span className="fa fa-star checked"></span>
-      <span className="fa fa-star checked"></span>
-      <span className="fa fa-star"></span>
-      <span className="fa fa-star"></span>
-      <span className="fa fa-star"></span>
-      {/* <div className="ec-stars-wrapper">
-	<a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
-	<a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
-	<a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
-	<a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
-	<a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
-  
-</div> */}
-      <h5>(5 stars) 10 reviews</h5>
-      <h5>{wine.description}</h5>
-      <p>cantidad</p>
-      <button className="flex flex-col items-center text-center8 bg-btn-color text-white py-4 px-8 hover:bg-red-600"  type="button"
-      >
-        Añadir al carrito
-      </button>
-      <br />
-      <br />
-      <button className="fflex flex-col items-center text-center bg-btn-color text-white py-4 px-8 hover:bg-red-600" type="button"
-      >
-        Añadir a lista de deseos
-      </button>
-      <div className="mb-3 pt-0">
-        <input type="text" placeholder="1" className="px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
+      <div className="flex justify-end p-2" >
+        <button className="bg-btn-color text-white py-4 px-8 hover:bg-red-600 " type="button"
+        >
+          Enviar
+        </button>
+
       </div>
-      <h1 className = "fw-full h-full flex flex-wrap self-center justify-center divide-x-2 gap-y-8 font-bold mt-10 mb-5" >RESEÑAS Y PUNTUACIONES</h1>
-      <h2 className = "fw-full h-full flex flex-wrap self-center justify-center divide-x-2 gap-y-8">Escribi una reseña</h2>
-      <div className="mb-3 pt-0">
-        <input type="text" placeholder="Escribir..." className="px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
-      </div>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-      <h5 className = "font-bold mt-10 mb-5" >Puntuar</h5>
-      <span className="heading"></span>
-      <span className="fa fa-star-o"></span>
-      <span className="fa fa-star-o"></span>
-      <span className="fa fa-star-o"></span>
-      <span className="fa fa-star-o"></span>
-      <span className="fa fa-star-o"></span>
-      <br />
-      <button className="fw-full h-full flex flex-wrap self-center justify-center divide-x-2 gap-y-8 bg-btn-color text-white py-4 px-8 hover:bg-red-600"  type="button"
-      >
-       Enviar
-      </button>
     </div>
 
   )
