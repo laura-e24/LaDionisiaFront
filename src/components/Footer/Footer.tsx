dbc56dd813cf21583a6abcfa94a53dd623039c7f
Luciano Federico Pereira <120595747+thisIsMySourceCode@users.noreply.github.com>
2023-01-13 15:02:27 -0300

    🦭 7th commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,13 +11,28 @@ export default function Footer() {
     }
     return (
         <footer className={`h-min w-full ${styles.bgfooter} p-14 flex footer`}>
-
           <Logo className="w-28 h-28"/>
-
           <div className="absolute fixed left-1/2 -translate-x-1/2 p-2">
-          <h1 className="text-xl text-gray-600 pb-2">Contact</h1>
+            <div class="grid grid-cols-2 gap-14">
+              <div>
+                <p className="text-gray-600 pb-1">ABOUT</p>
+                <p className="text-gray-600 pb-1">SHIPPING &amp; RETURNS</p>
+                <p className="text-gray-600 pb-1">NEWSLETTER</p>
+                <p className="text-gray-600 pb-1">PRIVACY</p>
+              </div>
+<div>
+<h1 className="text-xl text-gray-600 pb-2">Contact</h1>
           <p className="text-gray-600  pb-2">ladionisia@gmail.com</p>
           <p className="text-gray-600">+54 111 111 1111</p>
+   
+</div>
+
+
+
+          </div>
+
+
+
           </div>
         </footer>
     )
