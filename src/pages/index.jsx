836ff52ef792a60892d33e9ef7be799b6f089079
Sixtotk <sixtoorden@gmail.com>
2023-01-20 11:03:24 -0500

    RAMA DAÑADA NO USAR

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,41 @@
 import Head from 'next/head'
 import Home from '../components/Home/Home'
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import { useUser } from '@auth0/nextjs-auth0/client';
 import axios from 'axios';
+import CheckoutForm from "../components/payment";
 import { useRouter } from "next/router";
 function isUser(obj: any): obj is { '/roles': string[] } {
   return '/roles' in obj;
 }
 
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+
 export default function index() {
+  const [clientSecret, setClientSecret] = React.useState("");
+
+  React.useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   const { user, error, isLoading } = useUser();
   const router = useRouter();
   // console.log(user);
@@ -65,6 +93,11 @@ export default function index() {
           <Home />
           {/* guest */}
         </main>
+        {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
 
 
         <footer>
