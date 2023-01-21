ca9181e81bea523e2d64170919d86fc8e43c8e6b
elvis huanca <JHuanca_21@outlook.com>
2023-01-21 09:31:21 -0500

    fix deploy

--- src/pages/products/filters/[filter].tsx
+++ src/pages/products/filters/[filter].tsx
@@ -4,7 +4,7 @@ import Pagination from "../../../components/Pagination";
 import Card from "../../../components/Card/Card";
 import { useAppDispatch } from "../../../app/store";
 import { useSelector } from "react-redux";
-import { selectAllWines, selectAllWinesStatus, getAllWinesByContry, selectAllWinesByContry, selectAllWinesCountryStatus, setCurrentWines, selectCurrentWines, selectCountryFilter, cleanUpState, selectAllWinesFilters, getRegiones, filterByScore, selectAllFilters } from "../../../features/products/productsSlice";
+import { selectAllWines, selectAllWinesStatus, getAllWinesByContry, selectAllWinesByContry, selectAllWinesCountryStatus, setCurrentWines, selectCurrentWines, selectCountryFilter, cleanUpState, selectAllWinesFilters, getRegiones, selectAllFilters } from "../../../features/products/productsSlice";
 import { useEffect } from "react";
 import { EStateGeneric, filterWines } from "../../../utils/general";
 import { useRouter } from "next/router";
