57e148af03d6d224e09cb976b0ce48f353222273
Luciano Federico Pereira <120595747+thisIsMySourceCode@users.noreply.github.com>
2023-01-21 13:02:05 -0300

    cambios estilos v1.01

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -22,17 +22,17 @@ export default function Product() {
   const userExistente = users.find(u => u.email === user?.email)
   const currentUser = userExistente?.id
 
-  const Price = ({amount}) => {
-    let price   = (amount < 1) ? 100 : amount 
-    let entero  = Math.trunc(price);
-    let decimal = ""+(price-entero)*100
-        decimal = ("00" + decimal).slice(-2);
+  const Price = ({ amount }) => {
+    let price = (amount < 1) ? 100 : amount
+    let entero = Math.trunc(price);
+    let decimal = "" + (price - entero) * 100
+    decimal = ("00" + decimal).slice(-2);
     return (<><i className="-mt-8 "><small>$</small></i> {entero}<small><sup>{decimal}</sup></small></>);
   }
 
-  const WineDescription = ({text}) => {
-    let texto = ""+text;
-    const desc  = texto.split("(")
+  const WineDescription = ({ text }) => {
+    let texto = "" + text;
+    const desc = texto.split("(")
     texto = desc[0]
     desc[1] = (desc[1] == "") ? "" : "(" + desc[1]
     return (<>{texto}<small>{desc[1]}</small></>);
@@ -55,12 +55,12 @@ export default function Product() {
   }, [id])
   const productCurrent = id?.toString()
   function añadirfavoritos() {
-    dispatch(createFavorite({userId: currentUser, productId: productCurrent}))
+    dispatch(createFavorite({ userId: currentUser, productId: productCurrent }))
   }
 
   return (
-<>
-    <div className="
+    <>
+      <div className="
     main-body  
     pt-12 
     mb-12 
@@ -68,28 +68,28 @@ export default function Product() {
     max-w-screen-xl
     bg-bg-body 
     ">
-      <Navbar></Navbar>
-      <Link href="/products" >Volver</Link>
+        <Navbar></Navbar>
+        <Link href="/products" >Volver</Link>
 
-    <div className="w-2/3 float-right pt-4">
-    <p className="text-xl font-montserrat text-price-color">{wine.winery}</p>
-    <p className="text-4xl font-montserrat text-font-color" ><b>{wine.wine}</b></p>
-    <p className="text-4xl font-montserrat text-gray-600 pt-4 pb-4 price">
-      <span className="text-price-color">
-        <Price amount={wine.price}/>
-      </span> 
-      <span className="w-12 text-2xl ml-2 mt-4 pts">{wine.rating}<small>&nbsp;pts.</small></span>
-    </p>  
-    <p className="text-lg font-montserrat text-gray-600 wine-description">
-      <WineDescription text={wine.description}/>
-    </p>
-    <button className="wine-button p-2 border border-gray-600 w-2/12 self-center justify-self-end text-gray-600 ">TASTE IT</button>
-  </div>
-  <div className="w-1/3 h-96 flex justify-center items-center bg-product">
-    <img src={wine.image} alt={wine.wine} className="object-scale-down h-4/12" />
-  </div>
-  <Comments/>
-</div>
-<Footer></Footer>
-</>  )
+        <div className="w-2/3 float-right pt-4">
+          <p className="text-xl font-montserrat text-price-color">{wine.winery}</p>
+          <p className="text-4xl font-montserrat text-font-color" ><b>{wine.wine}</b></p>
+          <p className="text-4xl font-montserrat text-gray-600 pt-4 pb-4 price">
+            <span className="text-price-color">
+              <Price amount={wine.price} />
+            </span>
+            <span className="w-12 text-2xl ml-2 mt-4 pts">{wine.rating}<small>&nbsp;pts.</small></span>
+          </p>
+          <p className="text-lg font-montserrat text-gray-600 wine-description">
+            <WineDescription text={wine.description} />
+          </p>
+          <button className="wine-button p-2 border border-gray-600 w-18 self-center justify-self-end text-gray-600 ">TASTE&nbsp;IT</button>
+        </div>
+        <div className="w-1/3 h-96 flex justify-center items-center bg-product">
+          <img src={wine.image} alt={wine.wine} className="object-scale-down h-4/12" />
+        </div>
+        <Comments />
+      </div>
+      <Footer></Footer>
+    </>)
 }
