99ecd68c1bb1ea3ecdbc4f313c3a1da586cf7cbc
elvis huanca <JHuanca_21@outlook.com>
2023-01-23 10:21:07 -0500

    posible fix

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
-import Image                from 'next/image'
-import Link                 from 'next/link'        
-import NavBar               from '../Navbar/NavBar'
-import Footer               from '../Footer/Footer'
+import Image from 'next/image'
+import Link from 'next/link'
+import NavBar from '../Navbar/NavBar'
+import Footer from '../Footer/Footer'
 import React, { useEffect } from 'react';
 
 
@@ -9,12 +9,12 @@ import React, { useEffect } from 'react';
 const Home = () => {
   const goHome = (e) => {
     e.preventDefault()
-    const 
-         id = 'passion-for-wine',
-         element = document.getElementById(id)
-    let  y = element.getBoundingClientRect().top + window.pageYOffset 
-         y+= (window.innerWidth < 1200) ? 0 : -50 
-         window.scrollTo({top: y, behavior: 'smooth'})
+    const
+      id = 'passion-for-wine',
+      element = document.getElementById(id)
+    let y = element.getBoundingClientRect().top + window.pageYOffset
+    y += (window.innerWidth < 1200) ? 0 : -50
+    window.scrollTo({ top: y, behavior: 'smooth' })
     //  document
     //  .querySelector('#passion-for-wine')
     //  .scrollIntoView({block: "start", behavior: "smooth"})
@@ -22,42 +22,42 @@ const Home = () => {
   const goAge = (e) => {
     e.preventDefault()
     let modalAge = document.getElementById('modal-age')
-        modalAge.style.display = 'none'
-        localStorage.setItem('age', '18+')
+    modalAge.style.display = 'none'
+    localStorage.setItem('age', '18+')
   }
   useEffect(() => {
-  //  console.warn = () => {};
-    const  legalAge = localStorage.getItem('age')
-   if   ( !legalAge ) {
-      let  modalAge = document.getElementById('modal-age')
-           modalAge.style.display = 'inline-block'
-   } window.onresize = function(){ location.reload() }
-     const hash     = window.location.hash
-     if  ( hash ) {
-      setTimeout(()=> {
-         // document
-         // .querySelector(hash)
-         // .scrollIntoView({block: "start", behavior: "auto"})
-         const 
-         element = document.getElementById(hash)
-         let y = element.getBoundingClientRect().top + window.pageYOffset 
-             y+= (window.innerWidth < 1200) ? 0 : -50 
-         window.scrollTo({top: y, behavior: 'smooth'})
+    //  console.warn = () => {};
+    const legalAge = localStorage.getItem('age')
+    if (!legalAge) {
+      let modalAge = document.getElementById('modal-age')
+      modalAge.style.display = 'inline-block'
+    } window.onresize = function () { location.reload() }
+    const hash = window.location.hash
+    if (hash) {
+      setTimeout(() => {
+        // document
+        // .querySelector(hash)
+        // .scrollIntoView({block: "start", behavior: "auto"})
+        const
+          element = document.getElementById(hash)
+        let y = element.getBoundingClientRect().top + window.pageYOffset
+        y += (window.innerWidth < 1200) ? 0 : -50
+        window.scrollTo({ top: y, behavior: 'smooth' })
       }, 10)
     }
   })
   return (
-<><title>La Dionisia - Wines</title>
-<a onClick={goHome} href='#passion-for-wine' className="videologo top-4 w-28 h-28 absolute left-1/2 -translate-x-1/2 z-50">
-  <img src="assets/logo.svg"/>
-</a>
-<div className="video">
-<video autoPlay loop muted><source src="assets/video.mp4" type="video/mp4" /></video>
-</div>
-<a  href="#passion-for-wine" className="bottom-4 w-20 h-20 absolute left-1/2 -translate-x-1/2 opacity-50 z-50">
-  <img onClick={goHome} src="assets/scrolldown.gif"/>
-</a>  
-<div id="passion-for-wine" className="
+    <><title>La Dionisia - Wines</title>
+      <a onClick={goHome} href='#passion-for-wine' className="videologo top-4 w-28 h-28 absolute left-1/2 -translate-x-1/2 z-50">
+        <img src="assets/logo.svg" />
+      </a>
+      <div className="video">
+        <video autoPlay loop muted><source src="assets/video.mp4" type="video/mp4" /></video>
+      </div>
+      <a href="#passion-for-wine" className="bottom-4 w-20 h-20 absolute left-1/2 -translate-x-1/2 opacity-50 z-50">
+        <img onClick={goHome} src="assets/scrolldown.gif" />
+      </a>
+      <div id="passion-for-wine" className="
   main-body
   pt-12 
   mb-8
@@ -65,9 +65,9 @@ const Home = () => {
   max-w-screen-xl
   bg-bg-body 
 ">
-  <NavBar></NavBar>
-  <img src="assets/homeprov.webp" />         
-  <div className="
+        <NavBar></NavBar>
+        <img src="assets/homeprov.webp" />
+        <div className="
     w-full 
     flex 
     justify-around 
@@ -75,49 +75,51 @@ const Home = () => {
     mt-8
     wine-types
   ">
-    <a href='/products/type/rose'>
-    <div  className="rose text-center font-montserrat text-gray-600">
-      <div className='w-32 h-32 relative mb-2'>
-        <Image src="/assets/rose.png" layout='fill' />
-      </div>
-      Rose
-    </div>
-    </a>
-    <a href='/products/type/whites'>
-      <div className="white text-center font-montserrat text-gray-600">
-        <div className='w-32 h-32 relative mb-2'>
-          <Image src="/assets/white.png" layout='fill' />
+          <a href='/products/type/rose'>
+            <div className="rose text-center font-montserrat text-gray-600">
+              <div className='w-32 h-32 relative mb-2'>
+                <Image src="/assets/rose.png" layout='fill' />
+              </div>
+              Rose
+            </div>
+          </a>
+          <a href='/products/type/whites'>
+            <div className="white text-center font-montserrat text-gray-600">
+              <div className='w-32 h-32 relative mb-2'>
+                <Image src="/assets/white.png" layout='fill' />
+              </div>
+              White
+            </div>
+          </a>
+          <a href='/products/type/reds'>
+            <div className="red text-center font-montserrat text-gray-600">
+              <div className='w-32 h-32 relative mb-2'>
+                <Image src="/assets/red.png" layout='fill' />
+              </div>
+              Red
+            </div>
+          </a>
+          <a href='/products/type/sparkling'>
+            <div className="sparkling text-center font-montserrat text-gray-600">
+              <div className='w-32 h-32 relative mb-2'>
+                <Image src="/assets/sparkling.png" layout='fill' />
+              </div>
+              Sparkling
+            </div>
+          </a>
+          <a href='/products/type/dessert'>
+            <div className="dessert text-center font-montserrat text-gray-600">
+              <div className='w-32 h-32 relative mb-2'>
+                <Image src="/assets/dessert.png" layout='fill' />
+              </div>
+              Dessert
+            </div>
+          </a>
         </div>
-        White
+        <a id="home-main-1" href="/products/2889" title="wine list"></a>
+        <a id="home-main-2" href="/newsletter"></a>
+        <div id="modal-age"></div>
+        <Footer />
       </div>
-    </a>
-    <a href='/products/type/reds'>
-      <div className="red text-center font-montserrat text-gray-600">
-        <div className='w-32 h-32 relative mb-2'>
-          <Image src="/assets/red.png" layout='fill' />
-        </div>
-        Red
-      </div>
-    </a>
-    <a href='/products/type/sparkling'>
-      <div className="sparkling text-center font-montserrat text-gray-600">
-        <div className='w-32 h-32 relative mb-2'>
-          <Image src="/assets/sparkling.png" layout='fill' />
-        </div>
-        Sparkling
-      </div>
-    </a>
-    <a href='/products/type/dessert'>
-      <div className="dessert text-center font-montserrat text-gray-600">
-        <div className='w-32 h-32 relative mb-2'>
-          <Image src="/assets/dessert.png" layout='fill' />
-        </div>
-        Dessert
-      </div>
-    </a>
-  </div>
-  <a id="home-main-1" href="/products/2889" title="wine list"></a>
-  <a id="home-main-2" href="/newsletter"></a>
-  <Footer/>
-</div>
-</>)};export default Home;
+    </>)
+}; export default Home;
